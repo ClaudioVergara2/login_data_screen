7368656cdaacd3e7498ed1aee6edc3ec5ac8cff0
ClaudioVergara2 <claudio.vergara2@virginiogomez.cl>
2023-09-15 03:44:21 -0300

    Login y Crud Finalizado

--- Evaluacion1/Controllers/HomeController.cs
+++ Evaluacion1/Controllers/HomeController.cs
@@ -17,13 +17,28 @@ namespace Evaluacion1.Controllers
         {
             return View();
         }
-        public IActionResult DataScreen(Datos datos)
+        public IActionResult DataScreen(Datos datos, string usuario)
         {
             if (datos.nombres != "")
             {
                 ViewData["respuesta"] = datos;
             }
+            ViewData["usuario"] = usuario;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult ValidarLogin([FromBody] Login Login)
+        {
+            if ((Login.usuario == "ADMIN" && Login.contraseña == "ADMIN") ||
+                (Login.usuario == "Pedro" && Login.contraseña == "123456"))
+            {
+                return Json(true);
+            }
+            else
+            {
+                return Json(false);
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
